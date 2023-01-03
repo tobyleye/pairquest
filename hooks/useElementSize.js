22149f8214930412a6ee3ae9d368429cbcc56ff3
Oluwatobi <krisella74@gmail.com>
2023-01-03 14:14:55 +0100

    update element size on resize

--- hooks/useElementSize.js
+++ hooks/useElementSize.js
@@ -1,15 +1,38 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect, useMemo } from "react";
 
 export function useElementSize() {
-    const [element, setElement] = useState(null);
-    const [size, setSize] = useState({ width: null, height: null });
-    useEffect(() => {
-      if (element) {
-        const { clientHeight, clientWidth } = element;
-        setSize({ height: clientHeight, width: clientWidth });
-      }
-    }, [element]);
-  
-    return [size, setElement];
-  }
-  
+  const [element, setElement] = useState(null);
+  const [size, setSize] = useState({ width: null, height: null });
+
+  const observer = useMemo(
+    () =>
+      new window.ResizeObserver((entries) => {
+        if (entries[0]) {
+          const { clientHeight, clientWidth } = entries[0].target;
+          setSize({
+            width: clientWidth,
+            height: clientHeight,
+          });
+        }
+      }),
+    []
+  );
+
+  useEffect(() => {
+    if (element) {
+      const { clientHeight, clientWidth } = element;
+      setSize({ height: clientHeight, width: clientWidth });
+    }
+  }, [element]);
+
+  useEffect(() => {
+    if (!element) return;
+    observer.observe(element);
+    return () => {
+      observer.disconnect();
+    };
+  }, [element, observer]);
+
+
+  return [size, setElement];
+}
