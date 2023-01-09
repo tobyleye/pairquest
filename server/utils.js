4fb08e2d9ade930baadd809c93b123a3dababc2a
Oluwatobi <krisella74@gmail.com>
2023-01-09 22:07:08 +0100

    update shuffle function

--- server/utils.js
+++ server/utils.js
@@ -6,22 +6,23 @@ const range = (start, end) => {
   return res;
 };
 
-const shuffle = (array) => {
-  const swap = (array, fromIndex, toIndex) => {
-    let toItem = array[toIndex];
-    let fromItem = array[fromIndex];
-    array[toIndex] = fromItem;
-    array[fromIndex] = toItem;
+// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+const shuffle = (sequence) =>  {
+  const swap = (i, j) => {
+    const ith_item = sequence[i];
+    const jth_item = sequence[j];
+
+    sequence[i] = jth_item;
+    sequence[j] = ith_item;
   };
 
-  for (let i = 0; i < array.length; i++) {
-    let newIndex = Math.floor(Math.random() * array.length);
-    if (newIndex !== i) {
-      swap(array, i, newIndex);
-    }
+  for (let i = sequence.length - 1; i > 0; i--) {
+    const roll = Math.floor(Math.random() * i);
+    swap(roll, i);
   }
-  return array;
-};
+
+  return sequence;
+}
 
 const generateBoardItems = (gridSize, theme) => {
   gridSize = gridSize[0] * gridSize[1]
--- utils.js
+++ utils.js
@@ -6,28 +6,28 @@ const range = (start, end) => {
   return res;
 };
 
-const shuffle = (array) => {
-  const swap = (array, fromIndex, toIndex) => {
-    let toItem = array[toIndex];
-    let fromItem = array[fromIndex];
-    array[toIndex] = fromItem;
-    array[fromIndex] = toItem;
+// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+const shuffle = (sequence) =>  {
+  const swap = (i, j) => {
+    const ith_item = sequence[i];
+    const jth_item = sequence[j];
+
+    sequence[i] = jth_item;
+    sequence[j] = ith_item;
   };
 
-  for (let i = 0; i < array.length; i++) {
-    let newIndex = Math.floor(Math.random() * array.length);
-    if (newIndex !== i) {
-      swap(array, i, newIndex);
-    }
+  for (let i = sequence.length - 1; i > 0; i--) {
+    const roll = Math.floor(Math.random() * i);
+    swap(roll, i);
   }
-  return array;
-};
 
+  return sequence;
+}
 
 const generateBoardItems = (gridSize, theme) => {
-  gridSize = gridSize[0] * gridSize[1]
+  gridSize = gridSize[0] * gridSize[1];
   const totalPairs = gridSize / 2;
-  const rangeSize = theme === "icons" ? 8 : 99
+  const rangeSize = theme === "icons" ? 8 : 99;
   const source = range(0, rangeSize);
   let withReplacement = totalPairs > source.length;
   const items = [];
@@ -42,4 +42,4 @@ const generateBoardItems = (gridSize, theme) => {
   return shuffle(items);
 };
 
-export {  generateBoardItems };
+export { generateBoardItems };
