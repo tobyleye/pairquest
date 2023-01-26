2b73c5c6f9ec22ac2a342a154995bd9369bfd3ea
Oluwatobi <krisella74@gmail.com>
2023-01-26 23:04:53 +0100

    cleanup

--- pages/m/[id].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-import { MultiPlayerGame } from "../../components/multiplayer/game";
-
-export default function Page() {
-  const router = useRouter();
-  const { id } = router.query;
-
-  return <MultiPlayerGame room={id} />;
-}
